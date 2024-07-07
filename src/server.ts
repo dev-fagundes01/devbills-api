7f1841fc96cd68e49c6f0bd6670fd078e43f84ae
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2024-07-07 00:58:02 -0300

    Configuring Mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
+import "dotenv/config";
 import express, { json } from "express";
+import { setupMongo } from "./database";
 import { routes } from "./routes";
 
-const app = express();
+setupMongo().then(() => {
+	const app = express();
 
-app.use(json())
-app.use(routes)
+	app.use(json());
+	app.use(routes);
 
-app.listen(3333, () => console.log('🚀 app is running at port 3333'));
+	app.listen(3333, () => console.log("🚀 app is running at port 3333"));
+});
