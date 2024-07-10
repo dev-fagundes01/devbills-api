086e350416d90a58f89dea980b1489285bd246af
dfagundes2001 <fagundesdiego2015bolcombr@gmail.com>
2024-07-09 21:07:40 -0300

    feat: :sparkles: configure Mongoose to connect to MongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
+import "dotenv/config";
 import express, { json } from "express";
+import { setupMongo } from "./database";
 import { routes } from "./routes";
 
-const app = express();
+setupMongo().then(() => {
+	const app = express();
 
-app.use(json());
-app.use(routes)
-app.listen(3333, () => console.log("app is running at port 3333"));
+	app.use(json());
+	app.use(routes);
+	app.listen(3333, () => console.log("🚀app is running at port 3333"));
+});
